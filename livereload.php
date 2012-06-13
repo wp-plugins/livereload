aac94dbc2b3faa5a04a064013ba1db85c8cc69c0
functionsfile <functionsfile@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-13 15:07:04 +0000

    Initial commit
    git-svn-id: https://plugins.svn.wordpress.org/livereload/trunk@557360 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ livereload.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Plugin Name: LiveReload
+Plugin URI: http://wordpress.org/extend/plugins/livereload
+Description: Add the LiveReload javascript snippet to the footer of your theme.
+Version: 1.0
+Author: Barry Ceelen
+Author URI: http://functionsfile.com
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+add_action( 'wp_footer', 'fufi_livereload_snippet' );
+
+fufi_livereload_snippet() { ?>
+	<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+<?php } ?>
---
+++ readme.txt
@@ -0,0 +1,25 @@
+=== LiveReload ===
+Contributors: functionsfile
+Tags: LiveReload, javascript
+Requires at least: 1.2.0
+Tested up to: 3.3.2
+Stable tag: trunk
+
+Add the LiveReload javascript snippet to the footer of your theme.
+
+== Description ==
+
+[LiveReload](http://livereload.com/ "LiveReload") is a Mac OS X menu bar app for web developers. It monitors changes in folders you specify. When a file is saved, the browser will automatically refresh showing the changes.
+LiveReload also comes with various preprocessors baked in (LESS, SASS, Compass, Stylus, CoffeeScript, IcedCoffeeScript, Eco, SLIM, HAML, Jade).
+
+You can use LiveReload without [browser extensions](http://livereload.com/ "LiveReload Browser Extensions") by adding a javascript snippet to your page. This plugin does just that.
+
+== Installation ==
+
+1. Download the plugin archive and expand it.
+2. Put the 'livereload' directory in your WordPress plugin directory.
+3. Go to the Plugins page in your WordPress Administration area and click 'Activate' for LiveReload.
+
+== Changelog ==
+
+* 1.0 Initial Release
